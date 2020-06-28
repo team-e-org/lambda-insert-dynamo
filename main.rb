24af248c1ea792aa22f06ec975d0e17278107c13
Ryoma Kawahara <ryoma.k.0224@gmail.com>
2020-06-28 11:06:05 +0900

    select user ids related with tags

---
+++ main.rb
@@ -0,0 +1,50 @@
+require 'mysql2'
+require 'json'
+
+def lambda_handler(event:, context:)
+  json = JSON.parse(event)
+
+  tag_ids = []
+  json["tags"].each do |tag|
+    tag_ids << tag["id"]
+  end
+
+  p select_user_ids(tag_ids)
+end
+
+def build_where_clause(tag_ids)
+  return "" if tag_ids.length == 0
+
+  where_clause = "WHERE tag_id IN ("
+  tag_ids.each_with_index do |id, idx|
+    if (idx == tag_ids.length-1)
+      where_clause += "#{id})"
+    else
+      where_clause += "#{id}, "
+    end
+  end
+
+  where_clause
+end
+
+def select_user_ids(tag_ids)
+  client = Mysql2::Client.new(host: '127.0.0.1', username: 'test_user', password: 'password', database: 'test_db')
+
+  query = "
+    SELECT
+      DISTINCT(u.id) AS user_id
+    FROM
+      pins_tags AS pt
+      JOIN pins AS p ON pt.pin_id = p.id
+      JOIN users AS u ON p.user_id = u.id
+    #{build_where_clause(tag_ids)}
+  "
+
+  user_ids = []
+  client.query(query).each do |row|
+    user_ids << row['user_id']
+  end
+  user_ids
+end
+
+lambda_handler(event: "{\"pin\":{\"id\":1,\"userId\":1,\"title\":\"pin1\",\"description\":\"pin1 description\",\"imageUrl\":\"https://pinko-bucket.s3-ap-northeast-1.amazonaws.com/pins/009174cf-c4fe-4a49-a4b2-cb24a98887c9.png\",\"isPrivate\":false},\"tags\":[{\"id\":9,\"tag\":\"neko\"},{\"id\":1,\"tag\":\"sushi\"},{\"id\":12,\"tag\":\"inukkoro\"}]}", context: "")
